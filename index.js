8c20916bdb174a8340659eefe45da3f61556a586
Sophie Gu <sophieqgu@gmail.com>
2021-01-03 04:28:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 document.querySelector('main#main').remove()
 
 let newHeader = document.createElement('h1#victory')
-newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.appendChild(newHeader)
+newHeader.innerHTML = "Sophie is the champion"
